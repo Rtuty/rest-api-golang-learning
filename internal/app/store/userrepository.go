45e112392e1de666532af992d423aa110efd01c0
Kirill Kudryavcev <rtutiui@mail.ru>
2022-08-25 16:46:08 +0300

    Добавлены тесты

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -7,12 +7,24 @@ type UserRepository struct {
 }
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
-	if err := r.store.db.QueryRow("*INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id").Scan(&u.ID); err != nil {
+	if err := r.store.db.QueryRow(
+		"*INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
 		return nil, err
 	}
-	return nil, nil
+	return u, nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	u := &model.User{}
+	if err := r.store.db.QueryRow("SELECT ID, email, encrypted_password FROM users WHERE email = $1", email).Scan(
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
